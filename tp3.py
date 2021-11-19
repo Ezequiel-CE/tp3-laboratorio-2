217c7ec8ec10e7b4d6a01a2fdb86c0efcd3fabec
Ezequiel-CE <69295442+Ezequiel-CE@users.noreply.github.com>
2021-11-19 20:49:36 -0300

    added funcionalidad para  nuevo

--- tp3.py
+++ tp3.py
@@ -1,5 +1,5 @@
 
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic
 import sqlite3
 
@@ -10,10 +10,74 @@ class MiVentana(QMainWindow):
         super().__init__()
         uic.loadUi("base.ui", self)
         #coneccion a base de datos
-        self.conexion= sqlite3.connect("base3.db")
-        self.cursor= self.conexion.cursor()
+        # self.conexion= sqlite3.connect("base3.db")
+        # self.cursor= self.conexion.cursor()
+        self.nuevo.clicked.connect(self.on_nuevo)
+        self.aceptar.clicked.connect(self.on_aceptar)
+        self.cancelar.clicked.connect(self.on_cancelar)
+        self.lista.itemClicked.connect(self.on_item_clicked)
+        self.init_state()
         
-
+    def init_state(self):
+        self.form.hide()
+        self.form.setEnabled(False)
+        self.editar.setEnabled(False)
+        self.eliminar.setEnabled(False)
+        
+        
+    def on_nuevo(self):
+        if self.form.isVisible():
+            self.form.hide()
+            self.clear_inputs()
+        else:
+            self.form.show()
+            self.form.setEnabled(True)
+        
+            
+    def on_aceptar(self):
+        #info para la database
+        nombre = self.nombre.text()
+        apellido = self.apellido.text()
+        correo = self.correo.text()
+        telefono = self.telefono.text()
+        direccion = self.direccion.text()
+        nacimiento = self.nacimiento.text()
+        altura = self.altura.text()
+        peso = self.peso.text()
+        
+        self.lista.addItem(f"{nombre} {apellido}")
+        self.clear_inputs()
+        #habilita los botones
+        self.editar.setEnabled(True)
+        self.eliminar.setEnabled(True)
+        
+    def on_cancelar(self):
+        self.clear_inputs()
+        
+    def on_item_clicked(self):
+        self.confirm_panel.hide()
+        print(self.lista.currentItem().text())
+        self.nombre.setText(self.lista.currentItem().text())
+        self.form.setReadOnly(True)
+        
+        
+    def clear_inputs(self):
+        self.nombre.setText("")
+        self.apellido.setText("")
+        self.correo.setText("")
+        self.telefono.setText("")
+        self.direccion.setText("")
+        self.nacimiento.setText("")
+        self.altura.setText("")
+        self.peso.setText("")
+        
+        
+        
+        
+        
+        
+        
+        
 
 app = QApplication([])
 
